0233056baaca164a532b5437512c1b2909f23270
Neza Melencion <mmelencion@gmail.com>
2019-04-28 20:58:16 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <button @click="selectedComponent = 'appHome'">Home</button>
+        <button @click="selectedComponent = 'appAboutUs'">AboutUs</button>
+        <button @click="selectedComponent = 'appContactUs'">ContactUs</button>
+        <hr>
+        <p>{{ selectedComponent }}</p>
+        <keep-alive>
+          <component :is="selectedComponent">
+            <!-- <p>Default Content</p> -->
+          </component>
+        </keep-alive>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+import AboutUs from "./components/AboutUs.vue";
+import ContactUs from "./components/ContactUs.vue";
+
+export default {
+  data: function() {
+    return {
+      quoteTitle: "Home",
+      selectedComponent: "appHome"
+    };
+  },
+  components: {
+    appHome: Home,
+    appAboutUs: AboutUs,
+    appContactUs: ContactUs
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/ContactUs.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="title">
+      <slot name="title"></slot>
+      <span style="color: #ccc">
+        <slot name="subtitle">Contact Us</slot>
+      </span>
+    </div>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h2 {
+  color: red;
+}
+
+.title {
+  font-style: italic;
+}
+</style>
